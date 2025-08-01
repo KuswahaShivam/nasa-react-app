a903b369a356f5a7cf1979dd7a4f43808fc536cc
Shivam <sshivamkumar770@gmail.com>
2025-08-01 10:13:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import SideBar from "./components/Sidebar";
+
+function App() {
+  const [data, setData] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const [showModel, setShowModel] = useState(false);
+  function handleToggleModel() {
+    setShowModel(!showModel);
+  }
+  useEffect(() => {
+    async function fetchAPIData() {
+      const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+      const today = new Date().toDateString();
+      const localKey = `NASA-${today}`;
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        console.log("Fetched froom cache Today");
+        return;
+      }
+
+      localStorage.clear();
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData));
+        setData(apiData);
+        console.log("Fetched froom apiData Today");
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchAPIData();
+  }, []);
+
+  return (
+    <>
+      {data ? (
+        <Main data={data} />
+      ) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
+      {showModel && (
+        <SideBar data={data} handleToggleModel={handleToggleModel} />
+      )}
+      {data && <Footer data={data} handleToggleModel={handleToggleModel} />}
+    </>
+  );
+}
+
+export default App;
